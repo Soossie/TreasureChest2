e7214d6faf22a9721c4f223ee4af39701f8bf3e5
sandra-sanj <sandra.juursoo@metropolia.fi>
2024-09-27 12:26:32 +0300

    connector and database test

--- game.py
+++ game.py
@@ -1,8 +1,31 @@
-# this is a game
+# Treasure Chest (Aarrearkku)
 
-print('hei maailma!')
-print('moi')
-print("Hei, hei")
+import random
+from geopy import distance
+
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host='localhost',
+    port=3306,
+    database='treasure_chest',
+    user='treasure',
+    password='chest',
+    autocommit=True,
+    collation='utf8mb4_general_ci'
+)
 
 
+def database_function_test():
+    sql = "SELECT * FROM difficulty"
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(sql)
+    result = cursor.fetchall()
 
+    # tulostaa vaikeustaso-taulun kaikki rivit
+    for row in result:
+        print(row)
+    return
+
+
+database_function_test()
