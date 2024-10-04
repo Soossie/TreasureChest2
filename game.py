d147b5f1da486b208b5e93283e70631b6f5fd707
Ira <ira.kumpula@metropolia.fi>
2024-10-04 19:56:55 +0300

    added comments

--- game.py
+++ game.py
@@ -22,7 +22,8 @@ home_country = get_country_name(home_airport_icao)
 money = get_player_money(game_id)
 
 # aloitustilanne        #######tähän asti toimii
-print(f"You're in {home_country} at {home_airport}. You have {money} $. "
+print(f"You're in {home_country} at {home_airport}. You have {money} €. "
+#tässä välissä pitää antaa vihje aarremaasta: joku funktio hakee maan 1. kirjaimen tietokannasta?
       f"Where would you like to travel?\nOptions: ")
 
 country_list = []
@@ -40,7 +41,7 @@ def travel_between_countries():
         if airport_icao1 != airport_icao2:
             i += 1
             country_list.append([i, country, distance, ticket_cost])
-            # print(f'{i}. {country}, {distance} km, ticket costs {ticket_cost} $.\n')
+            # print(f'{i}. {country}, {distance} km, ticket costs {ticket_cost} €.\n')
             # country_list.append(country)
     print(tabulate(country_list, headers=["Number", "Country", "Distance (km)", "Ticket cost (€)"], tablefmt="pretty"))
 
@@ -58,10 +59,11 @@ def travel_inside_country():
         if airport_icao1 != airport_icao2:
             i += 1
             airport_list.append([i, airport, distance, ticket_cost])
-            # print(f'{i}. {airport}, {distance} km, ticket costs {ticket_cost} $.\n')
+            # print(f'{i}. {airport}, {distance} km, ticket costs {ticket_cost} €.\n')
             # airport_list.append(airport)
     print(tabulate(airport_list, headers=["Number", "Airport", "Distance (km)", "Ticket cost (€)"], tablefmt="pretty"))
 
+#^tulostuvaan taulukkoon pitää lisätä sarake "Enough money for the ticket (x)" tms.
 
 #laske maiden välisen lennon hinta etäisyyden perusteella
 def count_ticket_cost_between_countries(distance):
@@ -87,13 +89,14 @@ def count_ticket_cost_inside_country(distance):
         ticket_cost = 100 + 0.40 * distance
     return ticket_cost
 
-#Pelaaja valitsee ensimmäisen maan, jos oikea niin seuraava vaihe, jos väärä niin looppaa
+# pelaaja valitsee ensimmäisen maan, jos oikea niin seuraava vaihe, jos väärä niin looppaa
 travel_between_countries()
 next_country = int(input("Input country number: "))
 next_country -= 1
 if next_country not in range(len(country_list)):
     print("Select one of the countries from the list: ")
     next_country = input("")
+money -= ticket_cost    #päivitä pelaajan rahamäärä (money - ticket_cost)
 print(f"The ticket to {country_list[next_country]} costs {ticket_cost} and the distance there is {distance}. You have {money} left.")
 
 """
@@ -105,7 +108,10 @@ if next_country == treasure_land_country:
 else:
     print(f"You're traveling to {next_country} {next_country(get_default_airport_for_country)}. The treasure is not in this country.")
 
+#tähän pitää laittaa taas vihje aarremaasta (1. kirjain)
+
     print("Where would you like to travel to next? Input country country number. \n options:")
     for i in game_countries:
         print(i)
 """
+
