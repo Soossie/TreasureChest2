aae37146ce6484a54841682a2965b81efccec0b2
Soossi <elias.karikoski@hotmail.com>
2024-10-04 13:10:33 +0300

    nvm fuck uppasin

--- game_functions.py
+++ game_functions.py
@@ -251,7 +251,7 @@ def get_used_airport_coordinates(airport_icao):
 def get_distance_between_airports(airport_icao1, airport_icao2):
     coordinates1 = get_used_airport_coordinates(airport_icao1)
     coordinates2 = get_used_airport_coordinates(airport_icao2)
-    return int(distance.distance(coordinates1, coordinates2).km)
+    return distance.distance(coordinates1, coordinates2).km
 
 # hae pelaajan nykyinen sijainti
 def get_current_location(game_id):
