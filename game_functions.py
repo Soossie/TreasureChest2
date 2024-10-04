e4236acbf18320a70f72fcad4f2ea9d73c5014b0
Soossi <elias.karikoski@hotmail.com>
2024-10-04 13:08:27 +0300

    NYT TOIMII??

--- game_functions.py
+++ game_functions.py
@@ -251,7 +251,7 @@ def get_used_airport_coordinates(airport_icao):
 def get_distance_between_airports(airport_icao1, airport_icao2):
     coordinates1 = get_used_airport_coordinates(airport_icao1)
     coordinates2 = get_used_airport_coordinates(airport_icao2)
-    return distance.distance(coordinates1, coordinates2).km
+    return int(distance.distance(coordinates1, coordinates2).km)
 
 # hae pelaajan nykyinen sijainti
 def get_current_location(game_id):
@@ -259,5 +259,4 @@ def get_current_location(game_id):
     cursor = connection.cursor()
     cursor.execute(sql)
     result = cursor.fetchone()
-    result = int(result)
     return result[0]
