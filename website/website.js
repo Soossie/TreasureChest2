1a7bcf115d4e822466d8238e8442fceaf8773fc6
nurhana <nur.hana@metropolia.fi>
2024-12-04 14:19:27 +0200

    Lisäsin HTMl:n ja JavaScriptin Leaflet kartta, muokkasin CSS

---
+++ website/website.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const map = L.map('map', { tap: false });
+L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+  maxZoom: 20,
+  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+}).addTo(map);
+map.setView([60, 24], 7);
+let marker = L.marker([60.3172, 24.963]).addTo(map); // esimerkki marker
+
+/*
+//Jos haluamme, voimme lisätä openstreetmapin googlemapin sijaan.
+const map = L.map('map').setView([60.23, 24.74], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+let marker = L.marker([60.3172, 24.963]).addTo(map);
+ */
