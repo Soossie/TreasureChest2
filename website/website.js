56774e481ffdfa00bfa7c977287566d031a5865c
sandra-sanj <sandra.juursoo@metropolia.fi>
2024-12-05 20:16:53 +0200

    added flight info to airport markers

--- website/website.js
+++ website/website.js
@@ -9,6 +9,8 @@ L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
 }).addTo(map);
 //map.setView([60, 24], 7);
 
+const airportMarkers = L.featureGroup().addTo(map);
+
 //let marker = L.marker([60.3172, 24.963]).addTo(map); // esimerkki marker
 
 /*
@@ -21,8 +23,9 @@ let marker = L.marker([60.3172, 24.963]).addTo(map);
  */
 
 const apiUrl = 'http://127.0.0.1:3000/';
-const newGameUrl = apiUrl + '/new-game'
-const gameInfoUrl = apiUrl + '/game-info'
+const newGameUrl = apiUrl + '/new-game';
+const gameInfoUrl = apiUrl + '/game-info';
+const flyToUrl = apiUrl + '/fly-to';
 
 let gameId;
 let stillPlaying = true;
@@ -35,14 +38,16 @@ async function startNewGame() {
   
   const response = await fetch(newGameUrl + `/${playerName}` + `/${difficultyLevel}`);
   if (!response.ok) throw new Error('Invalid server input!');
-  return await response.json();
+  const gameData = await response.json();
+  gameSetup(gameData);
 }
 
-async function gameSetup(){
-  const gameData = await startNewGame();
+async function gameSetup(gameData){
+  //const gameData = await startNewGame();
   console.log(gameData);
-  updateStatus(gameData);
   gameId = gameData.game_info.id;
+  
+  updateStatus(gameData);
 }
 
 async function getGameInfo() {
@@ -69,11 +74,65 @@ function updateStatus(data) {
   document.querySelector('#co2').innerHTML = `${data.game_info.co2_consumed}`;
   document.querySelector('#clue').innerHTML = `${data.game_info.clue}`;
   
+  // tyhjentää kartan merkeistä
+  airportMarkers.clearLayers();
+  
   // karttamerkit
   let marker = L.marker([data.current_location_info.latitude, data.current_location_info.longitude]).addTo(map);
   map.setView([data.current_location_info.latitude, data.current_location_info.longitude], 4);
+  
+  // lisää kaikille lentokentille seuraavat kolme riviä:
+  const airport_info = data.available_airports_info[0];  // testiarvo: valitsee aina ensimmäisen kentän
+  airportMarkers.addLayer(marker);
+  addFlightInfoToMarker(airport_info, marker);
+  
   // lisää kaikki lentokentät tähän. löytyy datasta kohdasta available_airports_info. muuta eri värisiksi
   
+}
+
+// lentoinfo markerille ja lentonappi
+function addFlightInfoToMarker(airportInfo, marker) {
+  const flighInfoMarkerPopup = document.createElement('div');
+  flighInfoMarkerPopup.classList.add('flight-info-marker')
+  
+  // lentoinfo
+  const h4 = document.createElement('h4');
+  h4.innerHTML = airportInfo.name;
+  flighInfoMarkerPopup.append(h4);
+  
+  let distanceElem = document.createElement('p');
+  distanceElem.innerHTML = `Distance ${airportInfo.flight_info.distance} km`;
+  flighInfoMarkerPopup.append(distanceElem);
+  
+  let ticketCostElem = document.createElement('p');
+  ticketCostElem.innerHTML = `Ticket cost ${airportInfo.flight_info.ticket_cost} €`;
+  flighInfoMarkerPopup.append(ticketCostElem);
+  
+  let co2Elem = document.createElement('p');
+  co2Elem.innerHTML = `Co2 consumption ${airportInfo.flight_info.co2_consumption} kg`;
+  flighInfoMarkerPopup.append(co2Elem);
+  
+  // lentonappi
+  const flyButton = document.createElement('button');
+  flyButton.classList.add('button');
+  flyButton.innerHTML = 'Fly here';
+  //flighInfoMarkerPopup.append(flyButton);
+  
+  const flyButtonContainer = document.createElement('div');
+  flyButtonContainer.append(flyButton)
+  flighInfoMarkerPopup.append(flyButtonContainer);
+  
+  marker.bindPopup(flighInfoMarkerPopup);
+  
+  // event napille
+  flyButton.addEventListener('click', async function () {
+    const response = await fetch(flyToUrl + `/${gameId}/${airportInfo.icao}`);
+    if (!response.ok) {
+      throw new Error('Invalid server input!');
+    }
+    const data = await response.json();
+    updateStatus(data);
+  });
 }
 
 
